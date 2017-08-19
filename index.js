28d3c9463838f21ef0bbe383599bf0523422c605
Seren <SerenTera@users.noreply.github.com>
2017-08-19 15:41:40 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Command = require('command')
 
 //Defaults
 const NO_REPEATS=true,			//No continual repetition notifications from previously informed parties, based on leader to differentiate.
-	  PRETEND_LEGIT=false,		//Pretend you are legitimately searching with window open. Very experimental and this does not mean totally risk-free lul.
+	  PRETEND_LEGIT=true,		//Pretend you are legitimately searching with window open. Very experimental and this does not mean totally risk-free lul.
 	  SEARCH_INTERVAL=30000,	//Default interval to search for lfgs. In milsecs. (default=30000ms=30s)
 	  TRY_AGAIN_INTERVAL=200,	//Default interval to retry search for lfg if previous search fail. Put a small delay for this. (It is unnatural for you to search multiple times before the initial search has returned to your pc) 
 	  CHECK_MAX_MEMBER=true		//Check if the raid has max number of members already, and prevent notification if so. False sets max member to 31.
@@ -13,9 +13,10 @@ let lowerRange=60,				//Lower Level range to search for
 	
 	
 //Raid Maximum Member Numbers
-const raidList=['ab','vh','ai','hh','rally','event']	//List out the normally used terms for raids here (ONLY RAIDS)
-const raidMaxNumber=[7,7,10,20,30,30]					//List out corresponding raid max member number
+const raidList=['cw','ab','vh','ai','hh','rally','event'],	//List out the normally used terms for raids here (ONLY RAIDS)
+	  raidMaxNumber=[3,7,7,10,20,30,30]						//List out corresponding raid max member number
 
+	  
 module.exports = function lfgfinder(dispatch) {
 	const command = Command(dispatch)
 	
@@ -36,8 +37,6 @@ module.exports = function lfgfinder(dispatch) {
 	})
 	
 	command.add('lfgfind', args => { //investigate later how to use ...args 
-		if(PRETEND_LEGIT && searchterms.length===0) pretendlegit()
-		
 		if(args===undefined) leadlist=[] //reset leadlist if no argument
 		
 		args=args.split(',')
@@ -75,7 +74,6 @@ module.exports = function lfgfinder(dispatch) {
 		if(arg===undefined) {
 			clearTimeout(timer)
 			command.message('(LFG Finder) All searches stopped')
-			searching=false
 			searchterms=[]
 			searchno=[]
 			leadlist=[]
@@ -92,11 +90,9 @@ module.exports = function lfgfinder(dispatch) {
 			command.message('(LFG Finder) Left searches:'+searchterms)
 		}
 		
-		if(PRETEND_LEGIT && searchterms.length===0) {
-			searching=false
+		if(searchterms.length===0) {
 			leadlist=[]
 			clearTimeout(timer)
-			dispatch.toServer('C_PARTY_MATCH_WINDOW_CLOSED',1,{})
 		}
 	})
 	
@@ -107,7 +103,7 @@ module.exports = function lfgfinder(dispatch) {
 	
 	/////Dispatches
 	dispatch.hook('S_SHOW_PARTY_MATCH_INFO', 1, event => {
-		if(searching && !windowopened) { //Do not notify if its because window is opened.
+		if(searching && !windowopened) { 				//Do not notify if its because window is opened.
 			messages.push.apply(messages,event.listings) //Array object cannot use concat.
 			
 			/*if(event.pageCount!==event.pageCurrent) {
@@ -129,13 +125,14 @@ module.exports = function lfgfinder(dispatch) {
 			})
 			
 			messages=[]
+			dispatch.toServer('C_PARTY_MATCH_WINDOW_CLOSED',1,{})
 			searching=false
 		}
 	})	
 	
 	dispatch.hook('C_REQUEST_PARTY_MATCH_INFO', 'raw', fake => {
 		if(!fake) {
-			if(PRETEND_LEGIT && searchterms.length!==0 && !windowopened) {
+			if(PRETEND_LEGIT && !searching && !windowopened) { //while searching server will see it as your window is opened. This will close the window before allowing true opening.
 				dispatch.toServer('C_PARTY_MATCH_WINDOW_CLOSED',1,{})
 			}
 			
@@ -144,13 +141,7 @@ module.exports = function lfgfinder(dispatch) {
 	})
 	
 	dispatch.hook('C_PARTY_MATCH_WINDOW_CLOSED', 'raw', fake => {
-		if(!fake) {
-			if(PRETEND_LEGIT && searchterms.length!==0 && windowopened) {
-				pretendlegit()
-			}
-			
-			windowopened=false
-		}
+		if(!fake) windowopened=false
 	})
 	
 	dispatch.hook('S_RETURN_TO_LOBBY', 'raw', code => { //clear all timer when switching characters
@@ -162,14 +153,18 @@ module.exports = function lfgfinder(dispatch) {
 	/////Functions
 	function finder(){
 		if(!searching && !windowopened) {
-			dispatch.toServer('C_REQUEST_PARTY_MATCH_INFO', 1, {
-				unk1:0,
-				minlvl:lowerRange,
-				maxlvl:upperRange,
-				unk2:3,
-				unk3:0,
-				purpose:'' //cannot use purpose well because of capitalization specific searching (TERA PLEASE)
-			})
+			if(PRETEND_LEGIT) pretendlegit()
+			
+			else {
+				dispatch.toServer('C_REQUEST_PARTY_MATCH_INFO', 1, {
+					unk1:0,
+					minlvl:lowerRange,
+					maxlvl:upperRange,
+					unk2:3,
+					unk3:0,
+					purpose:'' //cannot use purpose well because of capitalization specific searching (TERA PLEASE)
+				})
+			}
 			searching=true
 			timer = setTimeout(finder, SEARCH_INTERVAL)
 		}
@@ -206,16 +201,16 @@ module.exports = function lfgfinder(dispatch) {
 	function pretendlegit() { //In Tera NA, opening the window send these packets in order. CRPMI,CRPMI,CRMPMI
 		dispatch.toServer('C_REQUEST_PARTY_MATCH_INFO', 1, {
 				unk1:0,
-				levelRangeLow:60,
-				levelRangeHigh:65,
+				levelRangeLow:lowerRange,
+				levelRangeHigh:upperRange,
 				unk2:3,
 				unk3:0,
 				purpose:''
 		})
 		dispatch.toServer('C_REQUEST_PARTY_MATCH_INFO', 1, {
 				unk1:0,
-				levelRangeLow:60,
-				levelRangeHigh:65,
+				levelRangeLow:lowerRange,
+				levelRangeHigh:upperRange,
 				unk2:3,
 				unk3:0,
 				purpose:''
